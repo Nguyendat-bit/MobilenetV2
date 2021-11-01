21d7266a928cf5621ee99b611351ec79d701276f
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-11-01 12:09:00 +0700

    update predict

--- predict.py
+++ predict.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from argparse import ArgumentParser
 import sys
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
 
@@ -12,7 +14,7 @@ import numpy as np
 if __name__ == '__main__':
     parser = ArgumentParser()
     
-    parser.add_argument('--MobilenetV1-folder', default= 'MobilenetV1',type= str)
+    parser.add_argument('--MobilenetV2-save', default= 'MobilenetV2.h5',type= str)
     parser.add_argument('--test-file', type= str, required= True)
     parser.add_argument('--image-size', default= 150, type= int)
     parser.add_argument('--rho',default= 1.0, type= float)
@@ -23,18 +25,18 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit(0)
 
-    print('---------------------Welcome to Mobilenet V1-------------------')
+    print('---------------------Welcome to Mobilenet V2-------------------')
     print('Author')
     print('Github: Nguyendat-bit')
     print('Email: nduc0231@gmail')
     print('---------------------------------------------------------------------')
-    print('Predict MobileNetV1 model with hyper-params:')
+    print('Predict MobileNetV2 model with hyper-params:')
     print('===========================')
     for i, arg in enumerate(vars(args)):
         print('{}.{}: {}'.format(i, arg, vars(args)[arg]))
 
     # Load model
-    model = load_model(args.MobilenetV1_folder)
+    model = load_model(args.MobilenetV2_save)
 
     # Load label 
     with open('label.json') as f:
@@ -45,7 +47,9 @@ if __name__ == '__main__':
     img = preprocessing.image.img_to_array(img) / 255.
     img = tf.expand_dims(img, axis= 0) # (batch, row, col, chanel)
 
-    result = model(img)
-    result = np.argmax(result.numpy())
+    result = model(img).numpy()
+    result_indice = np.argmax(result)
     print('---------------------Prediction Result: -------------------')
-    print('This image is {}'.format(indices_class[result]))
+    image = Image.open(args.test_file)
+    plt.imshow(image)
+    print(f'This image is {indices_class[result_indice]} - accuracy: {result[result_indice]}')
